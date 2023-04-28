ae683e37e8059962ea4ea977bb8374cdd34a5702
guo-zi-xin <1992059868@qq.com>
2023-04-28 19:10:12 +0800

    [func]:轻应用配置适配多个路由

--- src/views/smallapplication/index.vue
+++ src/views/smallapplication/index.vue
@@ -7,49 +7,66 @@
           <div>{{ tabsLabel }}</div>
         </template>
         <div>
-          <search-from :params="searchParams.params" :config="searchParams.config" @reset="reset" @query="onSearch" />
+          <search-from :params="searchParams.params" :config="searchParams.config" @reset="reset" @query="onSearch"/>
         </div>
         <p class="tr">
           <el-button @click="refresh" icon="refresh" type="primary" />
-          <el-button v-if="tabsFlag" @click="handleCreate" type="primary">
+          <el-button v-if="tabsFlag" @click="handleOperate(null)" type="primary">
             {{ $t('add') }}{{ $t('lightApplication') }}
           </el-button>
         </p>
         <Table :thead="tableConfig.thead" :data="tableConfig.list" :loading="tableConfig.loading" :page="tableConfig.page"
           :pageSize="tableConfig.pageSize" :pageChange="page => pageChange(page)" :total="tableConfig.total"
           :pageSizeChange="size => pageSizeChange(size)">
+          <template #treeid="scope">
+            <div>
+              <div v-if="(scope as any).row.treeid === 0">{{$t('tree')}}</div>
+              <div v-else>{{$t('global')}}</div>
+            </div>
+          </template>
           <template #operate="scope">
             <div>
-              <el-button v-if="!tabsFlag" type="primary" link @click="handleOpen">{{ $t('goTo') }}</el-button>
-              <el-button v-if="tabsFlag" type="primary" link>{{ $t('edit') }}</el-button>
-              <el-button v-if="tabsFlag" type="primary" link>{{ $t('delete') }}</el-button>
+              <el-button v-if="!tabsFlag" type="primary" link @click="handleOpen((scope as any).row)">
+                {{ $t('goTo') }}
+              </el-button>
+              <el-button v-if="tabsFlag" type="primary" link @click="handleOperate((scope as any).row)">
+                {{ $t('edit') }}
+              </el-button>
+              <el-button v-if="tabsFlag" type="primary" link @click="handleDelete((scope as any).row)">
+                {{ $t('delete') }}
+              </el-button>
             </div>
           </template>
         </Table>
       </el-card>
     </div>
   </div>
-  <RunTask :jobId="jobId"/>
+  <LightAppModal v-if="modalFlag" v-model="modalFlag" :treeId="String(treeId)" :info="info" @close="modalFlag = false"
+    @confirm="handleConfirm" />
 </template>
 
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, getCurrentInstance, onMounted, computed, ComponentInternalInstance,watch } from 'vue'
+import { defineComponent, reactive, toRefs, getCurrentInstance, onMounted, computed, ComponentInternalInstance, watch, onBeforeUpdate } from 'vue'
 import store from '@/store'
 import searchFrom from '@/components/search/index.vue'
 import Table from '@/components/table/index.vue'
-import RunTask from '@/views/implement/runtask/index.vue'
+import LightAppModal from './components/LightAppModal.vue'
 import {
   LIGHT_APPLICATION_TABLE_THEAD,
   LIGHT_APPLICATION_SEARCH_CONFIG,
+  GLOBAL_LIGHT_APPLICATION_SEARCH_CONFIG,
 } from './config'
 import {
-  getSmallApplication
+  getSmallApplication,
+  deleteVariableById,
+  deleteLightApp
 } from '@/api/smallapplication/index'
 import router from '@/router'
+import { ElMessageBox } from 'element-plus'
 
 export default defineComponent({
-  components: { Table, searchFrom, RunTask },
+  components: { Table, searchFrom, LightAppModal },
   setup() {
     const { proxy } = getCurrentInstance() as ComponentInternalInstance
     const treeId = computed(() => { return store.getters.treeId })
@@ -76,12 +93,16 @@ export default defineComponent({
           edit_user: '',
           create_time: '',
           edit_time: '',
+          jobname: '',
+          projectid: '',
         },
         config: LIGHT_APPLICATION_SEARCH_CONFIG,
       },
       tabsFlag: window.location.hash.includes('global'),
-      jobId: '',
-      selectTreeId:treeId.value
+      jobid: '',
+      selectTreeId: treeId.value,
+      info: null,
+      modalFlag: false,
     })
 
     // 获取轻应用列表
@@ -89,7 +110,9 @@ export default defineComponent({
       const params = {
         treeId: treeId.value
       }
-      const dataRet = await getSmallApplication(params)
+      state.tableConfig.loading = true
+      const dataRet = await getSmallApplication(params).catch(err => state.tableConfig.loading = false)
+      state.tableConfig.loading = false
       if (dataRet) {
         state.tableConfig.list = dataRet
         state.tableConfig.defaultList = dataRet
@@ -98,9 +121,11 @@ export default defineComponent({
     }
 
     // 添加轻应用 
-    const handleCreate = () => {
-
+    const handleOperate = (row: any) => {
+      state.modalFlag = true
+      state.info = row
     }
+
     // 刷新 
     const refresh = () => {
       getData()
@@ -116,6 +141,8 @@ export default defineComponent({
         edit_user: '',
         create_time: '',
         edit_time: '',
+        jobname: '',
+        projectid: '',
       }
       getData()
     }
@@ -130,7 +157,9 @@ export default defineComponent({
           ((item.create_user ? item.create_user : '').includes(state.searchParams.params.create_user)) &&
           ((item.edit_user ? item.edit_user : '').includes(state.searchParams.params.edit_user)) &&
           ((item.create_time ? item.create_time : '').includes(state.searchParams.params.create_time)) &&
-          ((item.edit_time ? item.edit_time : '').includes(state.searchParams.params.edit_time))
+          ((item.edit_time ? item.edit_time : '').includes(state.searchParams.params.edit_time)) && 
+          ((item.jobname ? item.jobname : '').includes(state.searchParams.params.jobname)) &&
+          ((item.projectid ? item.projectid : '').includes(state.searchParams.params.projectid))
       })
       state.tableConfig.list = list
       state.tableConfig.total = list.length
@@ -148,26 +177,55 @@ export default defineComponent({
     }
 
     const handleOpen = (row: any) => {
-      state.jobId = row.jobid
+      state.jobid = row.jobid
       state.selectTreeId = row.projectid
-      router.push('/implement/runtask')
+      router.push(`/implement/runtask?jobid=${row.jobid}&projectid=${row.projectid}`)
+    }
+
+    const handleDelete = (row: any) => {
+      return ElMessageBox.confirm(proxy.$t('lightAppMessage'), `${proxy.$t('delete')}${proxy.$t('lightApplication')}`, {
+        confirmButtonText: proxy.$t('confirm'),
+        cancelButtonText: proxy.$t('cancel'),
+        type: 'warning'
+      }).then(async () => {
+        await deleteVariableById(row.jobid)
+        const dataRet = await deleteLightApp(row.id)
+        if (dataRet) {
+          proxy.$notification('operationSuccess')
+          getData()
+        }
+      })
+    }
+
+    const handleConfirm = () => {
+      state.modalFlag = false
+      getData()
     }
 
     const defaultOperate = () => {
       if (state.tabsFlag) {
         state.tabsLabel = `${proxy.$t('edit')}/${proxy.$t('lightApplication')}`
+        state.tableConfig.thead.filter(item => ['treeid','projectid', 'title','jobname'].includes(item.prop)).forEach(item => item.hidden = false)
+        state.searchParams.config = GLOBAL_LIGHT_APPLICATION_SEARCH_CONFIG
       } else {
         state.tabsLabel = proxy.$t('lightApplication')
+        state.tableConfig.thead.filter(item => ['treeid','projectid', 'title','jobname'].includes(item.prop)).forEach(item => item.hidden = true)
+        state.searchParams.config = LIGHT_APPLICATION_SEARCH_CONFIG
       }
     }
+    onBeforeUpdate(() => {
+      state.tabsFlag = window.location.hash.includes('global')
+      defaultOperate()
+      getData()
+    })
 
     onMounted(() => {
       defaultOperate()
       getData()
     })
 
-    watch(()=> treeId.value, (acc, cur) => {
-      if (acc !==cur) {
+    watch(() => treeId.value, (acc, cur) => {
+      if (acc !== cur) {
         reset()
       }
     })
@@ -176,13 +234,15 @@ export default defineComponent({
       treeId,
       treeData,
       ...toRefs(state),
-      handleCreate,
+      handleOperate,
       refresh,
       pageChange,
       pageSizeChange,
       reset,
       onSearch,
       handleOpen,
+      handleDelete,
+      handleConfirm,
     }
   }
 })
