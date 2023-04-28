02376f81874b3547a055fe68fabba4b10c64e33d
guo-zi-xin <1992059868@qq.com>
2023-04-28 19:09:09 +0800

    [func]:轻应用配置 添加/编辑弹窗组件开发

--- src/api/interface/smallapplication.ts
+++ src/api/interface/smallapplication.ts
@@ -1,2 +1,15 @@
 import request from '@/utils/request'
 
+export interface VariableOperateInfo {
+  jobuuid: string,
+   data: {name?: string, value?: string, describe?: string}[]
+}
+
+export interface LightAppInfo {
+  describe?: string;
+  jobid?: string;
+  title?: string;
+  type?: string;
+  treeid?: string | string[] | number,
+  parameter?: string
+}
